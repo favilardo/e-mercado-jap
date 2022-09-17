0592fad61382067ff8c50235732dc08244b7d1db
Franco Vilardo <favilardo@gmail.com>
2022-09-17 02:17:48 -0300

    Comentarios agregados

--- js/product-info.js
+++ js/product-info.js
@@ -1,14 +1,16 @@
-//Armamos la URL de la categoria deseada con la constante de Productos "https://japceibal.github.io/emercado-api/cats_products/", su número guardado en localhost y el ".json" de EXT_TYPE
+//Armamos las URLs de las APIS de producto y comentarios.
 const SPECIFIC_PRODUCT_INFO_URL = PRODUCT_INFO_URL + localStorage.getItem("prodID") + EXT_TYPE;
 const SPECIFIC_PRODUCT_INFO_COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL + localStorage.getItem("prodID") + EXT_TYPE;
 
 
 
 document.addEventListener("DOMContentLoaded", function(){
+
+    //Fetch con getJSONData para la info del producto.
     getJSONData(SPECIFIC_PRODUCT_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            console.log(resultObj.data);
 
+            //cambio el símbolo para la moneda.
             let currencySymbol = "";
             if (resultObj.data.currency === "UYU") {
                 currencySymbol = "$";
@@ -16,8 +18,10 @@ document.addEventListener("DOMContentLoaded", function(){
                 currencySymbol = "USD";
             }
 
+            //regex para precio separado por decimales.
             let separatedPrice = resultObj.data.cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
+            //Empiezo a cargar info en variable.
             let productDataToAppend = `<h1 class="text-left p-4">${resultObj.data.name}</h1>
             <hr>
             <h2>Precio</h2>
@@ -31,6 +35,8 @@ document.addEventListener("DOMContentLoaded", function(){
             <h2>Imágenes ilustrativas</h2>
             <div class="images-row">
             `;
+
+            //For para cargar fotos.
             for(foto of resultObj.data.images){
                 productDataToAppend += `<div class="images-column">
                     <img src="${foto}" class="img-fluid">
@@ -39,25 +45,31 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             productDataToAppend += "</div>";
 
+            //Cargo información al DOM.
             document.getElementById('product-load').innerHTML = productDataToAppend;
-
-            //mostramos listado
         }
     });
+
+    //Fetch para info de comentarios.
     getJSONData(SPECIFIC_PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
         if (resultObj.status === "ok"){
-            console.log(resultObj.data);
 
+            //Empiezo a cargar info a variable.
             let commentDataToAppend = `<h2 class="py-4">Comentarios</h2>`;
 
+            //Creo condición por si hay o no hay comentarios.
             if (resultObj.data.length === 0) {
                 commentDataToAppend += `<div class="comment-box">
                 <p class="text-center">No hay comentarios aún</p>
                 </div>`;
             } else {
+
+                //For para cargar cada comentario.
                 for (let comment of resultObj.data) {
                     commentDataToAppend += `<div class="comment-box">
                         <p><strong>${comment.user}</strong> - ${comment.dateTime} - `;
+
+                    //For para cargar estrellas
                     for (let contador = 1; contador <= 5; contador++){
                         if (contador <= comment.score) {
                             commentDataToAppend += `<span class="fa fa-star checked"></span>`;
@@ -73,9 +85,8 @@ document.addEventListener("DOMContentLoaded", function(){
                 }
             }
 
+            //Cargo info al DOM
             document.getElementById('comments-load').innerHTML = commentDataToAppend;
         }
     });
 });
-
-//<p class="font-weight-normal text-end my-2">Cant.</p>
